b75d0dea05459eb2d392185362a5b4e782ce8e4f
Nathan Onaka <nateonaka@gmail.com>
2018-04-07 10:55:41 -1000

    Rough Landing page complete

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
+        <div className='languagepals-landing-background'>
         <Grid verticalAlign='middle' textAlign='center' container>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+          <Grid.Column width={6}>
+            <Header as={'h1'} inverted>Language Pals</Header>
+            <Header as={'h3'} inverted>Learn a new language by finding study partners fluent in the language you want!</Header>
+            <Header as={'h3'} inverted>Make an account and list the language you're fluent in and the language you would like to learn.  You can browse our directory of language speakers and contact them to schedule study sessions.  We provide a calendar so you can easily remember all your sessions!</Header>
           </Grid.Column>
 
         </Grid>
+        </div>
     );
   }
 }
