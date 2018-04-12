125f66264c050af62636a09ab78e779f0089677e
dickensc <charlieadickens@gmail.com>
2018-04-11 15:02:52 -1000

    working signup page
    working signup page

--- app/imports/ui/pages/Signup.jsx
+++ app/imports/ui/pages/Signup.jsx
@@ -1,44 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Form, Grid, Header, Message, Segment, Dropdown } from 'semantic-ui-react';
+import { Container, Grid, Header, Message, Segment } from 'semantic-ui-react';
 import { Accounts } from 'meteor/accounts-base';
-import {Profiles, ProfileSchema} from '../../api/profile/profile';
+import { Profiles } from '/imports/api/profile/profile';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
 import LongTextField from 'uniforms-semantic/LongTextField';
 import SelectField from 'uniforms-semantic/SelectField';
 import SubmitField from 'uniforms-semantic/SubmitField';
-import HiddenField from 'uniforms-semantic/HiddenField';
 import ErrorsField from 'uniforms-semantic/ErrorsField';
+import { Bert } from 'meteor/themeteorchef:bert';
+import SimpleSchema from 'simpl-schema';
+import { languageList } from '../../api/profile/languageList';
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const SignupSchema = new SimpleSchema({
+  email: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+  bio: { type: String, optional: true },
+  picture: { type: String, optional: true },
+  fluentLanguages: {
+    type: Array,
+    optional: true,
+  },
+  'fluentLanguages.$': {
+    type: String,
+    allowedValues: languageList,
+  },
+  practiceLanguages: {
+    type: Array,
+    optional: true,
+  },
+  'practiceLanguages.$': {
+    type: String,
+    allowedValues: languageList,
+  },
+});
+
 /**
  * Signup component is similar to signin component, but we attempt to create a new user instead.
  */
 export default class Signup extends React.Component {
+
   /** Initialize state fields. */
   constructor(props) {
     super(props);
-    this.state = { email: '', password: '', error: '', firstName: '', lastName: '', bio: '', picture: '', fluentLanguages: '', practiceLanguages: ''};
-    // Ensure that 'this' is bound to this component in these two functions.
-    // https://medium.freecodecamp.org/react-binding-patterns-5-approaches-for-handling-this-92c651b5af56
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.submit = this.submit.bind(this);
+    this.insertCallback = this.insertCallback.bind(this);
+    this.formRef = null;
   }
 
-  /** Update the form controls each time the user interacts with them. */
-  handleChange(e, { name, value }) {
-    this.setState({ [name]: value });
+  /** Notify the user of the results of the submit. If successful, clear the form. */
+  insertCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Registration failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Registration succeeded' });
+      this.formRef.reset();
+    }
   }
 
   /** Handle Signup submission using Meteor's account mechanism. */
-  handleSubmit() {
-    const { email, password, firstName, lastName, bio, picture, fluentLanguages, practiceLanguages} = this.state;
-    Accounts.createUser({ email, username: email, password, firstName, lastName, bio, picture, fluentLanguages, practiceLanguages}, (err) => {
-      if (err) {
-        this.setState({ error: err.reason });
-      } else {
-        // browserHistory.push('/login');
-      }
-    });
+  submit(data) {
+    const { email, password, firstName, lastName, bio, picture, fluentLanguages, practiceLanguages } = data;
+    Accounts.createUser({ email, username: email, password }, this.insertCallback);
+    Profiles.insert(
+{ email, password, firstName, lastName, bio, picture, fluentLanguages, practiceLanguages },
+        this.insertCallback,
+);
   }
 
   /** Display the signup form. */
@@ -49,41 +80,23 @@ export default class Signup extends React.Component {
           <Header as="h2" textAlign="center">
             Register your account
           </Header>
-          <AutoForm schema={ProfileSchema} onSubmit={this.handleSubmit()}>
+          <AutoForm ref={(ref) => { this.formRef = ref; }} schema={SignupSchema} onSubmit={this.submit}>
             <Segment>
               <TextField name='email'/>
-              <Form.Input
-                  label="Password"
-                  icon="lock"
-                  iconPosition="left"
-                  name="password"
-                  placeholder="Password"
-                  type="password"
-                  onChange={this.handleChange}
-              />
-              <TextField name='firstName' onChange={this.handleChange}/>
-              <TextField name='lastName' onChange={this.handleChange}/>
-              <LongTextField name='bio' onChange={this.handleChange}/>
-              <TextField name='picture' onChange={this.handleChange}/>
-              <SelectField name='fluentLanguages' onChange={this.handleChange}/>
-              <SelectField name='practiceLanguages' onChange={this.handleChange}/>
+              <TextField name='password'/>
+              <TextField name='firstName'/>
+              <TextField name='lastName' />
+              <LongTextField name='bio' />
+              <TextField name='picture' />
+              <SelectField name='fluentLanguages'/>
+              <SelectField name='practiceLanguages'/>
               <SubmitField value='Submit'/>
               <ErrorsField/>
-              <HiddenField name='email'/>
             </Segment>
           </AutoForm>
           <Message>
             Already have an account? Login <Link to="/signin">here</Link>
           </Message>
-          {this.state.error === '' ? (
-              ''
-          ) : (
-              <Message
-                  error
-                  header="Registration was not successful"
-                  content={this.state.error}
-              />
-          )}
         </Grid.Column>
       </Grid>
     </Container>;
