c582e2ef8c31237e61fe15c50a56e5495f26f417
Nathan Onaka <nateonaka@gmail.com>
2018-04-17 10:32:14 -1000

    recopied meeting page with directory

--- app/imports/ui/pages/Meeting.jsx
+++ app/imports/ui/pages/Meeting.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Table, Header, Loader } from 'semantic-ui-react';
-import { Stuffs } from '/imports/api/stuff/stuff';
-import StuffItem from '/imports/ui/components/StuffItem';
+import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Profiles } from '/imports/api/profile/profile';
+import Profile from '/imports/ui/components/Profile';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-class Meeting extends React.Component {
+class Directory extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -18,37 +18,27 @@ class Meeting extends React.Component {
   renderPage() {
     return (
         <Container>
-          <Header as="h2" textAlign="center">List Stuff</Header>
-          <Table celled>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Quantity</Table.HeaderCell>
-                <Table.HeaderCell>Condition</Table.HeaderCell>
-                <Table.HeaderCell>Edit</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {this.props.stuffs.map((stuff) => <StuffItem key={stuff._id} stuff={stuff} />)}
-            </Table.Body>
-          </Table>
+          <Header as="h2" textAlign="center" inverted>Pals</Header>
+          <Card.Group itemsPerRow={2}>
+            {this.props.profiles.map((profile, index) => <Profile key={index} profile={profile}/>)}
+          </Card.Group>
         </Container>
     );
   }
 }
 
-/** Require an array of Stuff documents in the props. */
-Meeting.propTypes = {
-  stuffs: PropTypes.array.isRequired,
+/** Require an array of Profile documents in the props. */
+Directory.propTypes = {
+  profiles: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
-  // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Stuff');
+  // Get access to Profile documents.
+  const subscription = Meteor.subscribe('Profiles');
   return {
-    stuffs: Stuffs.find({}).fetch(),
+    profiles: Profiles.find({active: true}).fetch(),
     ready: subscription.ready(),
   };
-})(Meeting);
+})(Directory);
